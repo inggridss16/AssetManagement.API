498dbe3c479fbda874364386a588b3f46269863b
inggridss16 <inggridss16@gmail.com>
2025-11-06 23:32:54 +0700

    add userscontroller to provide users records

---
+++ Controllers/UsersController .cs
@@ -0,0 +1,32 @@
+﻿using AssetManagement.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagement.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AssetManagementDbContext _context;
+
+        public UsersController(AssetManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.MstUsers
+                .Select(u => new { u.Id, u.Name })
+                .ToListAsync();
+
+            return Ok(users);
+        }
+    }
+}
